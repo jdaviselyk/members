6c582bbbb8d472fa7c8895cde2536a08702a328a
jjerriselyk <jjerriselyk@users.noreply.github.com>
2021-05-12 14:10:51 -0400

    started members

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,53 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql} from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+export const pageQuery = graphql`
+query Members {
+  allWpJdMember {
+    nodes {
+      JDTeamMembersFields {
+        description
+        fieldGroupName
+        image {
+          altText
+        }
+        
+      }
+title
+      id
+      
+    }
+  }
+}
+`;
+
+const IndexPage = ({ data }) => {
+
+const members = data.allWpJdMember.nodes;
+
+
+return(
+<Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+  {members.length && members.map(f => {
+    return <div key={f.id}>
+      <div>{f.title} 
+      </div>
+      <div dangerouslySetInnerHTML ={{__html: f.JDTeamMembersFields.description,}} />
+
+      <div dangerouslySetInnerHTML ={{__html: f.JDTeamMembersFields.image,}} />
+      </div>
+  })}
   </Layout>
 )
+}
+
+
+  
+
 
 export default IndexPage
